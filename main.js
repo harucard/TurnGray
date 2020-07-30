8231e5aaafadad9f6241ef9cfc65122f9dcdeb87
ROQUELSON FERREIRA SOUZA <roquelson@gmail.com>
2020-07-30 14:29:55 -0300

    Borda de recorte criada

--- main.js
+++ main.js
@@ -1,32 +1,102 @@
 (function(){
-var canvas = document.getElementById("my-canvas");
-var context = canvas.getContext("2d");
-var img = document.createElement("img");
-var btnGrayScale = document.querySelector("#grayscale");
-var oImgData,aPix,nPixLen,oGrayImgvar,oGrayImg,dataURL;
-var  btnDownload =document.getElementById("btn-download");
-
+var oImgData,aPix,nPixLen,oGrayImgvar,oGrayImg,dataURL,imageTest;
+var startX, endX, startY, endY;
+var currShape = 'rectangle';
 var hasText = true;
-var clearCanvas = function (){
-  if(hasText){
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    hasText = false;
-  }
-}
+var mouseIsDown =0;
+var x = "#7CFC00";
+var y =2;
 
+var img = document.createElement("img");
+var canvas = document.getElementById("my-canvas");
+var btnGrayScale = document.querySelector("#grayscale");
+var  btnDownload =document.getElementById("btn-download");
+var btnCutImage = document.getElementById("cutImage");
+
+var context = canvas.getContext("2d");
 
 context.textAlign="center";
 context.font = "30px Arial";
 context.fillText("Drop an image ", 960, 540);
-
-
-console.log(canvas.width)
 
 canvas.addEventListener("dragover",dragOver,false);
 canvas.addEventListener("drop",dropImage,false);
 img.addEventListener("load",loadingImage,false);
 btnGrayScale.addEventListener("click",turnImageGray,false);
 btnDownload.addEventListener("click",download,false);
+btnCutImage.addEventListener("click",cutImage,false);
+
+function cutImage(){
+  img.src =  canvas.toDataURL('image/png');
+  imageTest =img;
+ canvas.addEventListener("mousedown",mouseDown,false);
+ canvas.addEventListener("mousemove",mouseMove,false);
+ canvas.addEventListener("mouseup",mouseUp,false);
+}
+
+function mouseDown(evt){
+  mouseIsDown = 1;
+  var pos = getMousePos(canvas, evt);
+  startX = endX = pos.x;
+  startY = endY = pos.y;
+  if(currShape=='rectangle')
+  {
+      drawSquare(); //update on mouse-up
+  }
+}
+
+function mouseMove(evt){
+  if (mouseIsDown !== 0) {
+    var pos = getMousePos(canvas, evt);
+    endX = pos.x;
+    endY = pos.y;
+    drawSquare();
+}
+}
+
+function mouseUp(evt){
+ if(mouseIsDown !== 0){
+   mouseIsDown=0;
+   var pos =getMousePos(canvas,evt);
+   endX = pos.x;
+   endY = pos.y;
+   if(currShape=="rectangle"){
+     drawSquare();
+   }
+ }
+}
+
+function drawSquare(){
+  if(imageTest){
+    context.drawImage(imageTest,0,0);
+  }
+
+    var w = endX - startX;
+    var h = endY - startY;
+    var offsetX = (w < 0) ? w : 0;
+    var offsetY = (h < 0) ? h : 0;
+    var width = Math.abs(w);
+    var height = Math.abs(h);
+
+    context.beginPath();
+    context.globalAlpha=0.7;
+    context.rect(startX + offsetX, startY + offsetY, width, height);
+    context.fillStyle = x;
+    context.setLineDash([20,20]);
+    context.lineWidth = y;
+    context.strokeStyle = x;
+    context.stroke();
+}
+
+function getMousePos(canvas,evt){
+  var rect = canvas.getBoundingClientRect();
+  return{
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
+}
+
+
 
 function turnImageGray(){
   for (nPixel = 0; nPixel < nPixLen; nPixel += 4) {
@@ -34,15 +104,21 @@ function turnImageGray(){
   }
   setImageOnCanvas();
 }
+
+function clearCanvas(){
+  if(hasText){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    hasText = false;}
+}
+
 function setImageOnCanvas(){
+ // imageTest = oImgData;
   context.putImageData(oImgData,0,0);
 }
-function download(){
- 
- // oGrayImg = new Image();
- dataURL = canvas.toDataURL('image/png');
-  btnDownload.href = dataURL;
 
+function download(){
+ dataURL = canvas.toDataURL('image/png');
+ btnDownload.href = dataURL;
 }
 
 function dragOver(evt){
@@ -57,6 +133,7 @@ function dropImage(evt){
       var reader = new FileReader();
       reader.onload = function (evt) {
         img.src = evt.target.result;
+       
       };
       reader.readAsDataURL(file);
     }
@@ -68,11 +145,13 @@ function loadingImage(){
   clearCanvas();
   canvas.width = img.width;
   canvas.height = img.height;
+  //imageTest=img;
   context.drawImage(img,0,0,canvas.width,canvas.height);
   //context.drawImage(img,0,0);
   //context.drawImage(img,0,0,1920,1080,0,0,canvas.width,canvas.height);
+ //console.log("dataURL:"+ canvas.toDataURL('image/png'))
   oImgData = context.getImageData(0, 0, img.width, img.height);
-  
+ 
   aPix = oImgData.data;
   nPixLen = aPix.length;
 }
